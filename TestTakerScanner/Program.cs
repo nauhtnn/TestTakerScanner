96f289c9dab3327566918035a845cc7c4105a799
nauhtnn <thuan.nguyenngoc@live.com>
2019-03-04 00:38:56 +0700

    + post and get ocr result

---
+++ TestTakerScanner/Program.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TestTakerScanner
+{
+    static class Program
+    {
+        const bool useMethod2 = true;
+        // Replace <Subscription Key> with your valid subscription key.
+        const string subscriptionKey = "26b4d99217f645818a4d2e049f3865fc";
+
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        const string uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/ocr";
+        const string uriBase2 =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/recognizeText";
+
+        static void Main()
+        {
+            // Get the path and filename to process from the user.
+            //Console.WriteLine("Analyze an image:");
+            //Console.Write(
+            //    "Enter the path to the image you wish to analyze: ");
+            string imageFilePath = "Bich_Tuyen_180330_2.jpg";// Console.ReadLine();
+
+            if (File.Exists(imageFilePath))
+            {
+                // Call the REST API method.
+                Console.WriteLine("\nWait a moment for the results to appear.\n");
+                MakeAnalysisRequest(imageFilePath).Wait();
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid file path");
+            }
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the analysis of the specified image file by using
+        /// the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to analyze.</param>
+        static async Task MakeAnalysisRequest(string imageFilePath)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Request headers.
+                client.DefaultRequestHeaders.Add(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Request parameters. A third optional parameter is "details".
+                // The Analyze Image method returns information about the following
+                // visual features:
+                // Categories:  categorizes image content according to a
+                //              taxonomy defined in documentation.
+                // Description: describes the image content with a complete
+                //              sentence in supported languages.
+                // Color:       determines the accent color, dominant color, 
+                //              and whether an image is black & white.
+                string requestParameters = (useMethod2) ? "mode=Handwritten" : "language=en&detectOrientation=false";
+
+                // Assemble the URI for the REST API method.
+                string uri = ((useMethod2) ? uriBase2 : uriBase) + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                // Read the contents of the specified local image
+                // into a byte array.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                // Add the byte array as an octet stream to the request body.
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                //using (HttpContent content = new StringContent("https://raw.githubusercontent.com/nauhtnn/certData/master/task/Bich_Tuyen_180330_1.jpg", System.Text.Encoding.UTF8, "application/json"))
+                {
+                    // This example uses the "application/octet-stream" content type.
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Asynchronously call the REST API method.
+                    response = await client.PostAsync(uri, content);
+                }
+
+                // Asynchronously get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n\n{0}\n",
+                    JToken.Parse(contentString).ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            // Open a read-only file stream for the specified file.
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Read the file's contents into a byte array.
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+
+}
---
+++ TestTakerScanner/Program2.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+
+namespace TestTakerScanner
+{
+    static class Program2
+    {
+        static void Main()
+        {
+            MakeRequest();
+            //QueryText("https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/textOperations/d3d946e2-7b32-4843-9d35-fa09926f1cad");
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "26b4d99217f645818a4d2e049f3865fc");
+
+            // Request parameters
+            queryString["mode"] = "Printed";
+            var uri = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/recognizeText?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = GetImageAsByteArray("Bich_Tuyen_180330_2.jpg");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+            }
+
+            // Asynchronously get the JSON response.
+            string contentString = await response.Content.ReadAsStringAsync();
+            var operation_location = response.Headers.GetValues("Operation-Location").GetEnumerator();
+            operation_location.MoveNext();
+            Console.WriteLine(operation_location.Current);
+            QueryText(operation_location.Current);
+
+            // Display the JSON response.
+            Console.WriteLine(contentString);
+        }
+
+        static async void QueryText(string uri)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "26b4d99217f645818a4d2e049f3865fc");
+            var x = await client.GetAsync(uri);
+            var y = x.Content.ReadAsStringAsync().Result;
+            JToken parent = JToken.Parse(y).Last;
+            while (parent.First == parent.Last)
+                parent = parent.First;
+            JToken i = parent.First;
+            while(i != parent.Last)
+            {
+                Console.WriteLine(i.Value<string>("text"));
+                i = i.Next;
+            }
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            // Open a read-only file stream for the specified file.
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Read the file's contents into a byte array.
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
