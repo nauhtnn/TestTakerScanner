5a59420cb5747af3311708c189a87bf5045d9428
nauhtnn <thuan.nguyenngoc@live.com>
2019-03-30 01:19:18 +0700

    + incomplete GUI

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyLib.Class1.Init();
+        }
+
+        private void FileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    filePath.Text = file;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    filePath.Text = string.Empty;
+                    break;
+            }
+        }
+
+        private void PostImage_Click(object sender, RoutedEventArgs e)
+        {
+            MyLib.Class1.PostImageToGetURL(filePath.Text);
+            while(MyLib.Class1.textURL.Length == 0)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+            MyMessage.Text = "finished post";
+        }
+
+        private void GetText_Click(object sender, RoutedEventArgs e)
+        {
+            MyLib.Class1.GetImageText();
+            MyMessage.Text = MyLib.Class1.imgText.ToString();
+        }
+    }
+}
---
+++ MyLib/Class1.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+
+namespace MyLib
+{
+    public class Class1
+    {
+        public static string textURL;
+        public static StringBuilder imgText;
+        public static void Init()
+        {
+            textURL = string.Empty;
+            imgText = new StringBuilder();
+        }
+        public static async void PostImageToGetURL(string imgPath)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "26b4d99217f645818a4d2e049f3865fc");
+
+            // Request parameters
+            queryString["mode"] = "Printed";
+            var uri = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/recognizeText?" + queryString;
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = GetImageAsByteArray(imgPath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+            }
+
+            // Asynchronously get the JSON response.
+            string contentString = await response.Content.ReadAsStringAsync();
+            var operation_location = response.Headers.GetValues("Operation-Location").GetEnumerator();
+            operation_location.MoveNext();
+            textURL = operation_location.Current;
+        }
+
+        public static async void GetImageText()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "26b4d99217f645818a4d2e049f3865fc");
+            var x = await client.GetAsync(textURL);
+            var y = x.Content.ReadAsStringAsync().Result;
+            JToken parent = JToken.Parse(y).Last;
+            while (parent.First == parent.Last)
+                parent = parent.First;
+            JToken i = parent.First;
+            imgText.Clear();
+            while (i != parent.Last)
+            {
+                imgText.Append(i.Value<string>("text") + "\n");
+                i = i.Next;
+            }
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            // Open a read-only file stream for the specified file.
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Read the file's contents into a byte array.
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
